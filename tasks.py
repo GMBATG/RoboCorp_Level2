0ed0253ff688d2668b53ea2a2ff562e8ebfdda94
GMBATG <148881951+GMBATG@users.noreply.github.com>
2023-10-26 09:03:53 +0200

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -57,18 +57,20 @@ def fill_the_form(orderRow):
     #page.get_by_label("1. Head:").select_option(str(orderRow["Head"]))
 
     #Click BODY
-    if orderRow["Body"] == "1":
-         page.get_by_text("Roll-a-thor body").click()
-    elif orderRow["Body"] == "2":
-        page.get_by_text("Peanut crusher body").click()
-    elif orderRow["Body"] == "3":
-        page.get_by_text("D.A.V.E body").click()
-    elif orderRow["Body"] == "4":
-        page.get_by_text("Andy Roid body").click()
-    elif orderRow["Body"] == "5":
-        page.get_by_text("Spanner mate body").click()
-    elif orderRow["Body"] == "6":
-        page.get_by_text("Drillbit 2000 body").click()
+    # if orderRow["Body"] == "1":
+    #      page.get_by_text("Roll-a-thor body").click()
+    # elif orderRow["Body"] == "2":
+    #     page.get_by_text("Peanut crusher body").click()
+    # elif orderRow["Body"] == "3":
+    #     page.get_by_text("D.A.V.E body").click()
+    # elif orderRow["Body"] == "4":
+    #     page.get_by_text("Andy Roid body").click()
+    # elif orderRow["Body"] == "5":
+    #     page.get_by_text("Spanner mate body").click()
+    # elif orderRow["Body"] == "6":
+    #     page.get_by_text("Drillbit 2000 body").click()
+
+    page.locator(f"input[value='{str(orderRow['Body'])}']").click()    
 
     #Enter LEGS
     page.get_by_placeholder("Enter the part number for the legs").fill(str(orderRow["Legs"]))
